469664c74192a8ae7375a515717cf44c90522272
Lix Wei <w_weilix@hotmail.com>
2019-11-30 20:39:55 +0800

    feat: add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import { graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+
+export const query = graphql`
+  query about {
+    markdownRemark(fields: { slug: { eq: "/about-me/" } }) {
+      html
+      rawMarkdownBody
+      frontmatter {
+        title
+        date(formatString: "YYYY年M月D日")
+        tags
+      }  
+    }
+  }
+`;
+
+const AboutPage = ({ data }) => {
+  const post = data.markdownRemark;
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      <time>{post.frontmatter.date}</time>
+      <div dangerouslySetInnerHTML={{ __html: post.html }}/>
+    </Layout>
+  );
+}
+
+export default AboutPage;
