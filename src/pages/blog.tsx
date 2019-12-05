7e2ecf7763e67cd601214bb3a37c83bede600200
Lix Wei <w_weilix@hotmail.com>
2019-12-05 22:29:30 +0800

    add blog page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Layout from "../components/Layout";
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      what are they;
+    </Layout>
+  );
+}
+
+export default BlogPage;
