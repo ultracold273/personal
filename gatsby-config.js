b22160f383d7f7a1fe5cb13a1f036361884c1f0d
Lix Wei <w_weilix@hotmail.com>
2019-12-05 22:28:38 +0800

    feat: add helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
